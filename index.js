d21818120281160495fe78d733fb416b855fbbf7
joetoenails <69773836+joetoenails@users.noreply.github.com>
2021-04-20 20:35:44 -0400

    posts query + resolver

--- index.js
+++ index.js
@@ -3,15 +3,32 @@ const gql = require("graphql-tag");
 const mongoose = require("mongoose");
 const { MONGODB } = require("./config.js");
 
+const Post = require("./models/Post");
+// const User = require("./models/User")
+
 const typeDefs = gql`
+  type Post {
+    id: ID!
+    body: String!
+    createdAt: String!
+    username: String!
+  }
+
   type Query {
-    sayHi: String!
+    getPosts: [Post]
   }
 `;
 
 const resolvers = {
   Query: {
-    sayHi: () => "Hello world!",
+    async getPosts() {
+      try {
+        const posts = await Post.find();
+        return posts;
+      } catch (e) {
+        throw new Error(e);
+      }
+    },
   },
 };
 
