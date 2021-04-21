f342dc095f79721437dd19c0fc07445459651699
joetoenails <69773836+joetoenails@users.noreply.github.com>
2021-04-20 20:40:46 -0400

    nodemon script, refactored files

--- index.js
+++ index.js
@@ -3,34 +3,9 @@ const gql = require("graphql-tag");
 const mongoose = require("mongoose");
 const { MONGODB } = require("./config.js");
 
-const Post = require("./models/Post");
+const typeDefs = require("./graphql/typeDefs");
+const resolvers = require("./graphql/resolvers");
 // const User = require("./models/User")
-
-const typeDefs = gql`
-  type Post {
-    id: ID!
-    body: String!
-    createdAt: String!
-    username: String!
-  }
-
-  type Query {
-    getPosts: [Post]
-  }
-`;
-
-const resolvers = {
-  Query: {
-    async getPosts() {
-      try {
-        const posts = await Post.find();
-        return posts;
-      } catch (e) {
-        throw new Error(e);
-      }
-    },
-  },
-};
 
 const server = new ApolloServer({ typeDefs, resolvers });
 
